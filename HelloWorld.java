002168135ea31d6e64e7a2f423c8c2cbedeed742
Nguyễn Văn Lập <laptddz@example.com>
2024-09-04 01:18:43 +0700

    BaiTapDauTien

--- HelloWorld.java
+++ HelloWorld.java
@@ -18,11 +18,29 @@ public class HelloWorld {
         // System.out.println("My name is "+name+", age = "+age);
         // scanner.close();
         // System.out.println("First number : " + Math.sqrt(81 ));
-        System.out.println("Student: ");
-        String name = scanner.nextLine();
-        System.out.println("Point: ");
-        int point = scanner.nextInt();
-        System.out.println("Student: " + name + " point: " + point);
-        scanner.close();
+        //
+        //Lab:01
+        // System.out.println("Student: ");
+        // String name = scanner.nextLine();
+        // System.out.println("Point: ");
+        // int point = scanner.nextInt();
+        // System.out.println("Student: " + name + " point: " + point);
+        // scanner.close();
+
+        //Lab02:
+        // System.out.println("Nhập vào chiều dài: ");
+        // int chieu_dai = scanner.nextInt();
+        // System.out.println("Nhập vào chiều rộng: ");
+        // int chieu_rong = scanner.nextInt();
+        // System.out.println("Chu vi hình chữ nhật = "+ (chieu_dai + chieu_rong) * 2);
+        // System.out.println("Diện tích hình chữ nhật = " + chieu_dai * chieu_rong);
+        // System.out.println("Cạnh nhỏ nhất = " +Math.min(chieu_dai, chieu_rong));      
+        // scanner.close();
+
+        //Lab03:
+        // System.out.println("Nhập vào độ dài cạnh của khối lập phương: ");
+        // int number = scanner.nextInt();
+        // System.out.println("Thể tích khối lập phương là: " + Math.pow(3, number));
+        // scanner.close();
     }
 }
